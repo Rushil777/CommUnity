08fda9dfa88281285a48e7c9866b080574c78596
Vereshan Veron Palani <ST10025632@vcconnect.edu.za>
2024-10-30 20:20:27 +0200

    Enabled notifications on whetehr the users account has been approved or declined. I updated code on the following kotlin files : - ServiceProvider.kt - ServiceProviderRegisterActivity.kt - Customer.kt - CustomerRegisterActivity.kt   and added a kotlin class :  - MyFirebaseMessagingService.kt

--- app/src/main/java/za/co/varsitycollege/st10215473/community/data/Customer.kt
+++ app/src/main/java/za/co/varsitycollege/st10215473/community/data/Customer.kt
@@ -1,26 +1,23 @@
 package za.co.varsitycollege.st10215473.community.data
 
-
 import com.google.firebase.Timestamp
-import java.time.LocalTime
 import java.util.Date
 
-
 class Customer(
-    val id: String= "",
+    val id: String = "",
     val idNumber: String = "",
-    val name: String ="",
-    val surname: String ="",
-    val phoneNumber: String ="",
-    val email: String ="",
+    val name: String = "",
+    val surname: String = "",
+    val phoneNumber: String = "",
+    val email: String = "",
     val age: Int = 0,
     val dateOfBirth: Date? = null,
     val status: String = "",
     val dateSubmitted: Date? = null,
     val isOnline: Boolean = false,
     val lastMessage: String = "",
-    val lastMessageTime: Timestamp? = null
-
-){
-    constructor():this("", "", "", "", "", "", 0, null, "", null, false, "", null)
+    val lastMessageTime: Timestamp? = null,
+    val fcmToken: String = "" // Added FCM token field
+) {
+    constructor() : this("", "", "", "", "", "", 0, null, "", null, false, "", null, "")
 }
